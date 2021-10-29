3914f2a6e77435ff69c630ca03325e40def562d4
Joshua Shirley <joshua_shirley@hotmail.com>
2021-10-28 21:39:58 -0600

    Updated stats to display correctly in LG Removed monthly from object stats

--- js/stats.js
+++ js/stats.js
@@ -3,38 +3,20 @@ let stats = {
         days: 0,
         peak: 0,
         peakNovember: 0,
-        fullTimeDays: 0,
-        november: 0,
-        december: 0,
-        january: 0,
-        febuary: 0,
-        march: 0,
-        april: 0
+        fullTimeDays: 0
     },
     committed: {
         working: 0,
         peak: 0,
         peakNovember: 0,
         fulltime: 0,
-        private: 0,
-        november: 0,
-        december: 0,
-        january: 0,
-        february: 0,
-        march: 0,
-        april: 0
+        private: 0
     },
     static: function() {
         this.totals.days = dateDifference(specialDates["open date"], specialDates["close date"]);
         this.totals.peak = schedule.peak.length;
         this.totals.peakNovember = schedule.peak.filter(day => JSONtoDate(day).getMonth() == 10).length;
         this.totals.fullTimeDays = dateDifference(specialDates.Type[schedule.type].Requirements["Start Date"], specialDates["close date"]);
-        this.totals.november = this.daysInMonth("2021-11-07");
-        this.totals.december = this.daysInMonth("2021-12-01");
-        this.totals.january = this.daysInMonth("2022-01-01");
-        this.totals.febuary = this.daysInMonth("2022-02-01");
-        this.totals.march = this.daysInMonth("2022-03-01");
-        this.totals.april = this.daysInMonth("2022-04-01");
     },
     update: function() {
         this.committed.working = schedule.assigned.length;
@@ -42,12 +24,6 @@ let stats = {
         this.committed.peakNovember = schedule.assigned.filter(day => (schedule.peak.filter(day => (new Date(day).getMonth() == 10)).includes(day))).length;
         // Add Algorithm to get full time days
         this.committed.fulltime = schedule.assigned.filter(day => (dateDifference(specialDates.Type[schedule.type].Requirements["Start Date"], day) > 0)).length;
-        this.committed.november = schedule.assigned.filter(day => (new Date(day).getMonth() == 10)).length;
-        this.committed.december = schedule.assigned.filter(day => (new Date(day).getMonth() == 11)).length;
-        this.committed.january = schedule.assigned.filter(day => (new Date(day).getMonth() == 0)).length;
-        this.committed.february = schedule.assigned.filter(day => (new Date(day).getMonth() == 1)).length;
-        this.committed.march = schedule.assigned.filter(day => (new Date(day).getMonth() == 2)).length;
-        this.committed.april = schedule.assigned.filter(day => (new Date(day).getMonth() == 3)).length;
     },
     daysInMonth: function(yyyymmdd) {
         var d = JSONtoDate(yyyymmdd);
